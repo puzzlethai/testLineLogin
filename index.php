d28aee30c4c21c722a37c1177f51bf7eaffa4b9b
OZONE <iq_maze@yahoo.com>
2017-06-19 10:29:57 +0700

    test line login 5 add access control allow origin and method

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php header( 'Location: /index.html' ) ; 
-header('Content-type: text/json');
-  header('Access-Control-Allow-Origin: *');
-  $uri = 'http'. ($_SERVER['HTTPS'] ? 's' : null) .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-  echo('<p>This information has come from <a href="' . $uri . '">' . $uri . '</a></p>');
-   ?>
